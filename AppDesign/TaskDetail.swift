05c31081263108bd8bbc0b795bae7e280db45d7b
Emil Muzafarov <emilm24@nycstudents.net>
2025-04-11 11:33:08 -0400

    new ocommit

---
+++ AppDesign/TaskDetail.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetail.swift
+//  AppDesign
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+struct TaskDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetail()
+}
